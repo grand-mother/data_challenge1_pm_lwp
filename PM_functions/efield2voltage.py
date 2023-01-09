4d6f5d5494a46d114ab0364632113b094e597ec1
Lech Piotrowski <science@lwp.email>
2023-01-09 13:58:37 +0100

    Switched SN<->EW antenna arms. Rotating response in frequency now (cleaner code).

--- PM_functions/efield2voltage.py
+++ PM_functions/efield2voltage.py
@@ -9,22 +9,11 @@ from misc_functions import time_passed
 @lru_cache(maxsize=1)
 def read_antenna_files():
     time_passed(True)
-    # print("0.1")
     suffix = "_reshaped_float32.npy"
     # suffix = "_reshaped_compressed_float32.npz"
     table_ewarm_new = an.get_tabulated_mod(PM_config.PM_files_path + f"/GP300Antenna_EWarm_leff{suffix}")
-    # print("0.2")
     table_snarm_new = an.get_tabulated_mod(PM_config.PM_files_path + f"/GP300Antenna_SNarm_leff{suffix}")
-    # print("0.3")
     table_zarm_new = an.get_tabulated_mod(PM_config.PM_files_path + f"/GP300Antenna_Zarm_leff{suffix}")
-    # print("1")
-    # # print("0.1")
-    # table_ewarm_new = an.get_tabulated(PM_config.PM_files_path + "/GP300Antenna_EWarm_leff.npy")
-    # # print("0.2")
-    # table_snarm_new = an.get_tabulated(PM_config.PM_files_path + "/GP300Antenna_SNarm_leff.npy")
-    # # print("0.3")
-    # table_zarm_new = an.get_tabulated(PM_config.PM_files_path + "/GP300Antenna_Zarm_leff.npy")
-    # # print("1")
     print(time_passed())
 
     return table_ewarm_new, table_snarm_new, table_zarm_new
@@ -52,8 +41,8 @@ def efield2voltage_pm(traces_t, e_theta, e_phi, freqs, sampling_time=0.5, **kwar
     dt = sampling_time
 
     # The interpolations below take almost all the time in this function
-    lt1, lp1 = an.get_interp_mod(table_ewarm_new, Zenith, Azimuth, N, dt * 1e-9)
-    lt2, lp2 = an.get_interp_mod(table_snarm_new, Zenith, Azimuth, N, dt * 1e-9)
+    lt1, lp1 = an.get_interp_mod(table_snarm_new, Zenith, Azimuth, N, dt * 1e-9)
+    lt2, lp2 = an.get_interp_mod(table_ewarm_new, Zenith, Azimuth, N, dt * 1e-9)
     lt3, lp3 = an.get_interp_mod(table_zarm_new, Zenith, Azimuth, N, dt * 1e-9)
     lt = np.array([lt1, lt2, lt3]).T
     lp = np.array([lp1, lp2, lp3]).T
@@ -81,24 +70,23 @@ def efield2voltage_pm(traces_t, e_theta, e_phi, freqs, sampling_time=0.5, **kwar
         vec_out = np.matmul(rot_in, vec_in.T)
         return vec_out[:2]  # not interested in E_r, neglibible
 
+    # E_traces_fft = np.array(np.fft.rfft(np.moveaxis(traces_t, -2, -1)), dtype='complex')  # fft , shape(2,N)
+    E_traces_fft = np.array(np.fft.rfft(traces_t), dtype='complex')  # fft , shape(2,N)
+
     # Fold antenna response in and get Voc
     # ToDo: Do the same stuff for 2D traces as for 3D traces array
     if traces_t.ndim<3:
-        E_tp_t = np.zeros([N, 2])  # xyz to theta-phi
-        for k in range(N):
-                E_tp_t[k] = xyz_thetaphi(traces_t[:, k], Zenith, Azimuth)
+        E_tp_fft = np.zeros([len(freqs), 2], dtype='complex') # xyz to theta-phi
+        for k in range(len(freqs)):
+            E_tp_fft[k] = xyz_thetaphi(E_traces_fft[:, k], Zenith, Azimuth)
     else:
         rm = R.from_matrix(rotationmatrix(Zenith, Azimuth).T)
         # rotation can be applied only on (x,3) arrays, so we have to do a lot of reshaping back and forth
-        ee = np.moveaxis(traces_t, 1, 2)
-        E_tp_t = rm.apply(ee.reshape(-1, 3)).reshape((traces_t.shape[0], -1, 3))
+        ee = np.moveaxis(E_traces_fft, 1, 2)
+        E_tp_fft = rm.apply(ee.reshape(-1, 3)).reshape((traces_t.shape[0], -1, 3))
 
-    E_tp_fft = np.array(np.fft.rfft(np.moveaxis(E_tp_t, -2, -1)), dtype='complex')  # fft , shape(2,N)
 
-    # ======Open circuit voltage of air shower=================
-
-    Voc_shower_complex = np.moveaxis(lt * E_tp_fft[..., 0, :][..., np.newaxis] + lp * E_tp_fft[..., 1, :][..., np.newaxis], -2, -1)
-
+    Voc_shower_complex = np.moveaxis(lt * E_tp_fft[..., 0, np.newaxis] + lp * E_tp_fft[..., 1, np.newaxis], -2, -1)
     Voc_shower_t = np.fft.irfft(Voc_shower_complex, n=N)  # (3,N)
 
     print(time_passed())
